d70dd987099e8e5cd87593a204b603ca37f5d138
szmuk <adamszmuk@gmail.com>
2016-11-29 23:32:01 +0100

    Added index

---
+++ Server/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        CarDatabaseEntities1 db = new CarDatabaseEntities1();
+
+        public ActionResult Index()
+        {
+            ViewBag.Cars = db.Cars.ToList();
+            ViewBag.Details = db.Details.ToList();
+            ViewBag.Errors = db.Errors.ToList();
+
+            return View();
+        }
+    }
+}
