f5608c1d2e86d496e4c0187046c058df079f9bb6
szmuk <adamszmuk@gmail.com>
2016-12-01 23:41:57 +0100

    index page

--- Server/Controllers/HomeController.cs
+++ Server/Controllers/HomeController.cs
@@ -14,11 +14,19 @@ namespace Server.Controllers
 
         public ActionResult Index()
         {
+            db.Cars.ToList();
             ViewBag.Cars = db.Cars.ToList();
             ViewBag.Details = db.Details.ToList();
             ViewBag.Errors = db.Errors.ToList();
 
             return View();
+        }
+
+        public ActionResult Test(string asd)
+        {
+            int a = 2;
+
+            return null;
         }
     }
 }
